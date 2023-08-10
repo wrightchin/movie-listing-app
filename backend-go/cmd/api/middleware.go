ec50ccf2d3f556b19dfd34cfb70032c2a8ec1759
wrightchin <mschincsk@gmail.com>
2023-08-10 15:21:26 +0800

    yaml update

--- backend-go/cmd/api/middleware.go
+++ backend-go/cmd/api/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://frontend-cityu-demo-go.apps.ocp4.lab.local")
 
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
